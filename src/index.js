9a340470ee91291859dff3fa82603505e6475038
pleuian <skies.thousand@gmail.com>
2020-10-15 18:28:07 +0300

    feat: add reverseInt function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (int) {
+    return (
+        parseFloat (
+            int
+                .toString()
+                .split('')
+                .reverse()
+                .join('')
+        )
+    )
 }
